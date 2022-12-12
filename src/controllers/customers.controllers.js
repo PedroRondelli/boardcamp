5f8d0d97700d55ed0b8989e982bf1bd22b64cdb5
Pedro Rondelli <pedrorondelli@hotmail.com>
2022-12-12 09:19:07 -0300

    feat: seachr for customers that init with a substring cpf

--- src/controllers/customers.controllers.js
+++ src/controllers/customers.controllers.js
@@ -2,6 +2,14 @@ import { connection } from "../database/database.js";
 
 export async function getCustomers(req, res) {
   const customers = await connection.query("SELECT * FROM customers;");
-  console.log(customers.rows);
-  res.sendStatus(200);
+  const { cpf } = req.query;
+
+  if (cpf) {
+    const consumersFound = customers.rows.filter((customer) =>
+      customer.cpf.startsWith(cpf)
+    );
+    return res.send(consumersFound);
+  }
+
+  res.send(customers.rows);
 }
